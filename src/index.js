ed80b2e76e984979aafc6cd40d542f6c1082e147
Alexey Gvozdev <gvozdeveloper@yandex.ru>
2023-12-19 16:52:05 +0300

    change files

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 import 'normalize.css'
-import './styles/main.scss'
+import './styles/main.scss';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
